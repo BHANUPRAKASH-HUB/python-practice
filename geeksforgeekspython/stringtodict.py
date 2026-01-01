17cf523f19858fcac6fb8450e478cf4bd45049a8
BHANUPRAKASH-HUB <bhanuprakashn07@gmail.com>
2026-01-01 20:40:53 +0530

    palindrome number

--- geeksforgeekspython/stringtodict.py
+++ geeksforgeekspython/stringtodict.py
@@ -1,3 +1,6 @@
-words_str=input("enter the words with spaces:")
-words={word:len(word) for word in words_str.split()}
-print(words)
+# words_str=input("enter the words with spaces:")
+# words={word:len(word) for word in words_str.split()}
+# print(words)
+# x,y=input("enter two values:").split()
+# print(x,y,end='\n',sep='\n')
+print("hello\t\t\tbhai\b\bkal")
