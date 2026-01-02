a58f1397ca7ea50bdc140aa8615e1130b5490962
BHANUPRAKASH-HUB <bhanuprakashn07@gmail.com>
2026-01-02 20:20:28 +0530

    swapping two numbers

---
+++ geeksforgeekspython/swapping_two_nums.py
@@ -0,0 +1,12 @@
+num1=int(input("enter number one:"))
+num2=int(input("enter number two:"))
+#by not using temp var
+num1,num2=num2,num1
+print(num1,num2)
+''' 
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+
+
+'''
