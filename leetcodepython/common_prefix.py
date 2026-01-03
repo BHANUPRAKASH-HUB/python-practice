c667ac4661156e76333323dab6d03d4db70ba409
BHANUPRAKASH-HUB <bhanuprakashn07@gmail.com>
2026-01-03 20:47:38 +0530

    common prefix longest

---
+++ leetcodepython/common_prefix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for i in range(len(prefix)):
+            for word in strs:
+                if i == len(word) or prefix[i] != word[i]:
+                    return prefix[:i]
+
+        return prefix
